031407e5925614301ccae5d2b65cc7d89a8f1f15
tskorohod <tskorohod@lmteam.by>
2023-01-04 22:19:29 +0300

    in process

--- learn-react2/src/App.js
+++ learn-react2/src/App.js
@@ -1,8 +1,11 @@
+import MainPage from './components/mainPage';
+
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
+			<MainPage></MainPage>
 		</div>
 	);
 }
--- learn-react2/src/components/mainPage.jsx
+++ learn-react2/src/components/mainPage.jsx
@@ -1,1 +1,54 @@
+const { useEffect, useState } = require("react");
+
 const API_URL = 'https://hacker-news.firebaseio.com/v0/';
+
+const MainPage = () => {
+
+	// const[] = useEffect();
+	const [inputValue, setinputValue] = useState(" ");
+
+	const sendInfo = async () => {
+		let response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json', {
+			method: "POST",
+			headers: {
+			"Content-Type": "application/json;charset=utf-8",
+			},
+			body: JSON.stringify({ body: inputValue }),
+		});
+
+		
+		console.log(response);
+	};
+
+	sendInfo();
+
+	// fetch(`${ API_URL }topstories.json`)
+	// 	.then(res => res.json())
+	// 	.then(ids => {
+	// 		return fetchItems(ids);
+	// 	})
+	// 	.then(response => console.log(response))
+	// 	.catch(() => {});
+
+	// const fetchItems = (ids) => {
+	// 	return Promise.all(
+	// 		ids.slice(0, 30).map(
+	// 			id => fetchItem(id)
+	// 		)
+	// 	);
+	// }
+
+	// const fetchItem = (id) => {
+	// 	return fetch(`${ API_URL }item/${id}.json?print=pretty`)
+	// 		.then(res => res.json())
+	// }
+
+
+	return (
+		<ul>
+
+		</ul>
+	)
+}
+
+export default MainPage;
