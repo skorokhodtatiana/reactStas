1673f302e62afe10afdf59b551fe682f3175f286
tskorohod <tskorohod@lmteam.by>
2023-01-08 21:22:36 +0300

    in process

--- learn-react2/src/components/mainPage.jsx
+++ learn-react2/src/components/mainPage.jsx
@@ -4,48 +4,68 @@ const API_URL = 'https://hacker-news.firebaseio.com/v0/';
 
 const MainPage = () => {
 
-	// const[] = useEffect();
-	const [inputValue, setinputValue] = useState(" ");
+	//const [inputValue, setinputValue] = useEffect();
+	const [inputValue, setinputValue] = useState([]);
 
-	const sendInfo = async () => {
-		let response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json', {
-			method: "POST",
-			headers: {
-			"Content-Type": "application/json;charset=utf-8",
-			},
-			body: JSON.stringify({ body: inputValue }),
-		});
-		console.log(response);
-	};
+	//let selectNews;
+		// fetch(`${API_URL}topstories.json`)
+		// 	.then(res => res.json())
+		// 	.then(ids => {
+		// 		return fetchIds(ids)
+		// 	})
+		// 	.catch(()=> {})
 
-	sendInfo();
+		// 	const fetchIds = (ids) => {
+		// 		setinputValue(ids);
+				
+		// 		const date = Promise.all(
+		// 			ids.slice(0, 5).map(id => fetchItem(id))
+		// 		)
+		// 		console.log(date);
+				
+		// 		setinputValue(date);
+				
+		// 	}
 
-	// fetch(`${ API_URL }topstories.json`)
-	// 	.then(res => res.json())
-	// 	.then(ids => {
-	// 		return fetchItems(ids);
-	// 	})
-	// 	.then(response => console.log(response))
-	// 	.catch(() => {});
-
-	// const fetchItems = (ids) => {
-	// 	return Promise.all(
-	// 		ids.slice(0, 30).map(
-	// 			id => fetchItem(id)
-	// 		)
-	// 	);
-	// }
-
-	// const fetchItem = (id) => {
-	// 	return fetch(`${ API_URL }item/${id}.json?print=pretty`)
-	// 		.then(res => res.json())
-	// }
+		// 	const fetchItem = async (id) => {
+		// 		return await fetch(`${ API_URL }item/${id}.json?print=pretty`)
+		// 			.then(res => res.json())
+		// 	}
+		useEffect(() => {
+			fetch(`${ API_URL }topstories.json`)
+				.then(res => res.json())
+				.then(ids => {
+					return fetchItems(ids);
+				})
+				.then(response => console.log(response))
+				.catch(() => {});
+	
+			const fetchItems = (ids) => {
+				const selectNews = Promise.all(
+					ids.slice(0, 5).map(
+						id => fetchItem(id)
+					)
+				);
+				setinputValue(selectNews);
+				console.log(selectNews);
+			}
+	
+			const fetchItem = (id) => {
+				return fetch(`${ API_URL }item/${id}.json?print=pretty`)
+					.then(res => res.json())
+					//.then(res => console.log(res))
+			}	
+		})
 
 
 	return (
-		<ul>
-
-		</ul>
+		<>
+		{/* <ul>
+			{inputValue.map((new) => (
+				<li>{new.by}</li>
+			))}
+		</ul> */}
+		</>
 	)
 }
 
