57ec54e195058d39eedee909175d3ab3d52e77f6
tskorohod <tskorohod@lmteam.by>
2023-01-09 22:12:23 +0300

    showing list of news done

--- learn-react2/src/components/mainPage.jsx
+++ learn-react2/src/components/mainPage.jsx
@@ -1,93 +1,40 @@
 const { useEffect, useState } = require("react");
 
-const API_URL = 'https://hacker-news.firebaseio.com/v0/';
-
 const MainPage = () => {
+	const API_URL = 'https://hacker-news.firebaseio.com/v0/';
 
-	const [inputValue, setinputValue] = useState('');
-	// const [inputValue, setinputValue] = useState({
-	// 	by: "",
-	// 	descendants: "",
-	// 	kids: "",
-	// 	score: "",
-	// });
+	const [inputValue, setinputValue] = useState([]);
 
-	let selectNews = [];
-		// fetch(`${API_URL}topstories.json`)
-		// 	.then(res => res.json())
-		// 	.then(ids => {
-		// 		return fetchIds(ids)
-		// 	})
-		// 	.catch(()=> {})
+	useEffect(() => {
+		fetch(`${ API_URL }topstories.json`)
+			.then(res => res.json())
+			.then(ids => {
+				return fetchItems(ids);
+			})
+			.then(response => setinputValue(response))
+			.catch(() => {});
 
-		// 	const fetchIds = (ids) => {
-		// 		setinputValue(ids);
-				
-		// 		const date = Promise.all(
-		// 			ids.slice(0, 5).map(id => fetchItem(id))
-		// 		)
-		// 		console.log(date);
-				
-		// 		setinputValue(date);
-				
-		// 	}
+		const fetchItems = (ids) => {
+			return Promise.all(
+				ids.slice(0, 30).map(
+					id => fetchItem(id)
+				)
+			);
+		}
 
-		// 	const fetchItem = async (id) => {
-		// 		return await fetch(`${ API_URL }item/${id}.json?print=pretty`)
-		// 			.then(res => res.json())
-		// 	}
-		useEffect(() => {
-			fetch(`${ API_URL }topstories.json`)
+		const fetchItem = async(id) => {
+			return await fetch(`${ API_URL }item/${id}.json?print=pretty`)
 				.then(res => res.json())
-				.then(ids => {
-					return fetchItems(ids);
-				})
-				// .then(response => setinputValue(response))
-				.catch(() => {});
-	
-			const fetchItems = (ids) => {
-				const selectNews = Promise.all(
-					ids.slice(0, 5).map(
-						id => fetchItem(id)
-					)
-				);
-				//setinputValue(selectNews);
-				//console.log(selectNews);
-			}
-	
-			const fetchItem = async(id) => {
-				//console.log(id);
-				setinputValue(id);
-				const result = await fetch(`${ API_URL }item/${id}.json?print=pretty`)
-					.then(res => res.json())
-					.then(res => {return res})
-					.then(res => {setinputValue(res)});
-					//.then(res => {console.log(res)})
-					//.then(response => setinputValue(response));
-					console.log(result)
-					//selectNews.push(result);
-			}	
-			//setinputValue(fetchItem);
-			// selectNews.push(fetchItem);
-
-			// setinputValue(fetchItem)
-			// 			console.log(fetchItem());
-		}, [])
-
+		}
+	}, [])
 
 	return (
 		<>
-		<ul>
-			{/* {inputValue.map((ne) => (
-				<li>{ne.by}</li>
-			))} */}
-			{<li>
-				<div>-{inputValue.by}</div>
-				<div>-{inputValue.descendants}</div>
-				<div>-{inputValue.kids}</div>
-				<div>-{inputValue.score}</div>
-			</li>}
-		</ul>
+		<ol>
+			{inputValue.map((result) => (
+				<li key={result.id}>{result.title + " " + result.score + " " +  result.by + " " +  result.time}</li>
+			))}
+		</ol>
 		</>
 	)
 }
