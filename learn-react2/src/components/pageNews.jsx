5dbc1b8e52cbe9d3e00d3ac95e5cbecf4bb7f67d
tskorohod <tskorohod@lmteam.by>
2023-01-23 22:40:51 +0300

    added styles

--- learn-react2/src/components/pageNews.jsx
+++ learn-react2/src/components/pageNews.jsx
@@ -1,10 +1,14 @@
+import './pageNews.css';
+
 const PageNews = () => {
 	return(
 		<>
-		<h1>Название: </h1>
-		<div>Содержание: </div>
-		<a>Ссылка на оригинал</a><br></br>
-		<button>Комментарии</button>
+		<div className="news">
+			<h1>Название: </h1>
+			<div className="news__content">Содержание: </div>
+			<a>Ссылка на оригинал</a><br></br>
+			<button className="news__comments">Комментарии</button>
+		</div>
 		</>
 	)
 }
