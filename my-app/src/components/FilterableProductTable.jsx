e702be5f99b47e318fef397c53af1220f8b3cc64
tskorohod <tskorohod@lmteam.by>
2024-10-17 23:51:12 +0300

    added state

--- my-app/src/components/FilterableProductTable.jsx
+++ my-app/src/components/FilterableProductTable.jsx
@@ -1,13 +1,26 @@
 import SearchBar from './SearchBar';
 import ProductTable from './ProductTable';
 import PRODUCTS from '../date';
+import { useState } from 'react';
 
 const FilterableProductTable = () => {
+	const [filterText, setFilterText] = useState('');
+	const [inStockOnly, setInStockOnly] = useState(false);
+
 	return (
 		<>
 			<div>
-				<SearchBar></SearchBar>
-				<ProductTable products={PRODUCTS}></ProductTable>
+				<SearchBar
+					filterText={filterText}
+					inStockOnly={inStockOnly}
+					onFilterTextChange={setFilterText}
+					onInStockOnlyChange={setInStockOnly}
+					></SearchBar>
+				<ProductTable
+					products={PRODUCTS}
+					filterText={filterText}
+					inStockOnly={inStockOnly}
+				></ProductTable>
 			</div>
 		</>
 	);
--- my-app/src/components/SearchBar.jsx
+++ my-app/src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
-const SearchBar = () => {
+const SearchBar = ({filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange}) => {
 	return(
 		<>
 			<form>
-				<input type="text" placeholder="Search..."></input>
-				<input type="checkbox" id="stoxkGoods"></input>
+				<input type="text" value={filterText} onChange={(e) => onFilterTextChange(e.target.value)} placeholder="Search..."></input>
+				<input type="checkbox" value={inStockOnly} onChange={(e) => onInStockOnlyChange(e.target.value)} id="stoxkGoods"></input>
 				<label htmlFor="stoxkGoods">Only show products in stock</label>
 			</form>
 		</>
