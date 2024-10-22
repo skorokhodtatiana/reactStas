d78dd1ad2157361734c08f1f27ef86ccb32f32f5
tskorohod <tskorohod@lmteam.by>
2024-10-22 22:49:56 +0300

    started to do tic tac toe

--- ticTacToe/tictactoe/src/App.js
+++ ticTacToe/tictactoe/src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Square({value, onSquareClick}) {
 	return (
-		<></>
+		<>
+			<button className="square" onClick={onSquareClick}>{value}</button>
+		</>
 	);
 }
 
-export default App;
+function Board() {
+	const [square, setSquare ] = useState(Array(9).fill(null));
+	const handleClick = () => {
+		setSquare('X');
+		// e.target.value = newValue;
+	}
+
+	return (
+		<>
+			<div className="board-row">
+				<Square value={square[0]} onSquareClick={handleClick}/>
+				<Square value={square[1]} onSquareClick={handleClick}/>
+				<Square value={square[2]} onSquareClick={handleClick}/>
+			</div>
+			<div className="board-row">
+				<Square value={square[3]} onSquareClick={handleClick}/>
+				<Square value={square[4]} onSquareClick={handleClick}/>
+				<Square value={square[5]} onSquareClick={handleClick}/>
+			</div>
+			<div className="board-row">
+				<Square value={square[6]} onSquareClick={handleClick}/>
+				<Square value={square[7]} onSquareClick={handleClick}/>
+				<Square value={square[8]} onSquareClick={handleClick}/>
+			</div>
+		</>
+	);
+}
+
+export default Board;
